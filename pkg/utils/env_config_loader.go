c916c76750f904577d05ecbfb5cb70cb8a9c93fe
HicaroD <hdanrlley1@gmail.com>
2025-01-06 21:57:12 -0300

    feat: adding new util for reading environment variables

---
+++ pkg/utils/env_config_loader.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"os"
+	"strconv"
+)
+
+// TODO: use package "reflect" for reading field tags from
+// struct and retrieve the environment variables
+//
+// The idea is to be really simple because environment
+// variables are generally strings or integers
+
+func EnvGetInt(key string, fallback int) int {
+	valStr, set := os.LookupEnv(key)
+	if !set {
+		return fallback
+	}
+
+	val, err := strconv.Atoi(valStr)
+	if err != nil {
+		return fallback
+	}
+
+	return val
+}
+
+func EnvGetString(key string, fallback string) string {
+	val, set := os.LookupEnv(key)
+	if !set {
+		return fallback
+	}
+	return val
+}
