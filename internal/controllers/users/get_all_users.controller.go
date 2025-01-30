8de2e26bacefdd5acab65f038386c91686a584bc
HicaroD <hdanrlley1@gmail.com>
2025-01-30 15:10:32 -0300

    fix: renaming file

--- internal/controllers/users/get_all_users.controller.go
+++ internal/controllers/users/get_all_users.controller.go
@@ -8,5 +8,14 @@ import (
 
 func (h *Handler) GetAllUsersController(ctx echo.Context) error {
 	// TODO: call some sort of service here
+
+	// TODO: build a package around it for later usage
+	// cookie := new(http.Cookie)
+	// cookie.Name = "user_id"
+	// cookie.Value = "102012"
+	// cookie.Secure = true
+	// cookie.HttpOnly = true
+	// ctx.SetCookie(cookie)
+
 	return ctx.JSON(http.StatusOK, map[string]string{"Hello from": "GetAllUsersController"})
 }
