82ec226a38e3db6440c5ce2db058b71a286caa33
HicaroD <hdanrlley1@gmail.com>
2025-02-06 20:10:47 -0300

    feat: avoiding cors problem

--- server/middlewares.go
+++ server/middlewares.go
@@ -8,4 +8,10 @@ import (
 func registerDefaultMiddlewares(e *echo.Echo) {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"}, // For development only
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE, echo.OPTIONS},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+	}))
+
 }
